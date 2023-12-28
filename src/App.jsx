96cf25745f3ab047938582b13358af199b2ad323
Abhayp01 <prajapatiabhay2003@gmail.com>
2023-12-28 17:28:34 +0530

    Update2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/head/header.jsx'
+import Header from './components/head/Header.jsx'
 import Cards from './components/Cards.jsx'
 function App() {
 
